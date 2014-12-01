195c9987f522aefdc16b8b827b4d64e61deb2297
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2014-12-01 09:42:33 -0500

    Boring using basic channel

--- boring.go
+++ boring.go
@@ -6,16 +6,18 @@ import (
 	"time"
 )
 
-func boring(msg string) {
+func boring(msg string, c chan string) {
 	for i := 0; ; i++ {
-		fmt.Println(msg, i)
+		c <- fmt.Sprintf("%s %d", msg, i)
 		time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
 	}
 }
 
 func main() {
-	go boring("boring")
-	fmt.Println("I'm listening")
-	time.Sleep(2 * time.Second)
+  c := make(chan string)
+	go boring("boring", c)
+  for i := 0; i < 5; i++ {
+    fmt.Printf("You say: %q\n", <-c)
+  }
   fmt.Println("You're boring; I'm leaving")
 }
