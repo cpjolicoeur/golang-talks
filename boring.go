5d3ef2c79499ff3d979e094ea7cdd1ec554468f0
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2014-12-01 10:53:35 -0500

    Boring - select with timeout

--- boring.go
+++ boring.go
@@ -7,35 +7,42 @@ import (
 )
 
 func multiplex(input1, input2 <-chan string) <-chan string {
-  c := make(chan string)
-  go func() {
-    for {
-      select {
-      case s := <-input1: c <- s
-      case s := <-input2: c <- s
-      }
-    }
-  }()
-  return c
+	c := make(chan string)
+	go func() {
+		for {
+			select {
+			case s := <-input1:
+				c <- s
+			case s := <-input2:
+				c <- s
+			}
+		}
+	}()
+	return c
 }
 
 func boring(msg string) <-chan string {
-  c := make(chan string)
+	c := make(chan string)
 
-  go func() {
-    for i := 0; ; i++ {
-      c <- fmt.Sprintf("%s %d", msg, i)
-      time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
-    }
-  }()
+	go func() {
+		for i := 0; ; i++ {
+			c <- fmt.Sprintf("%s %d", msg, i)
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+		}
+	}()
 
 	return c
 }
 
 func main() {
-  c := multiplex(boring("Han"), boring("Chewie"))
-  for i := 0; i < 20; i++ {
-    fmt.Println(<-c)
-  }
-  fmt.Println("Boring conversation anyways...")
+	c := boring("Han")
+	for {
+		select {
+		case s := <-c:
+			fmt.Println(s)
+		case <-time.After(500 * time.Millisecond):
+			fmt.Println("You're too slow.")
+			return
+		}
+	}
 }
